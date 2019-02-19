e15fd3145fda65576437d2f8cddd882988007a9e
Jack <cow12369@gmail.com>
2019-02-19 11:01:31 +0800

    2019_02_19

---
+++ git_command.sh
@@ -0,0 +1,7 @@
+url="https://github.com/JackingChen/lstm_turn_taking_prediction_ADOS.git"
+git init
+git add README.md
+git remote add origin $url
+git add .
+git commit -m  "first commit"
+git push -u origin master
---
+++ lstm_turn_taking_prediction_ADOS_5foldCV/Get_MJ_reuslt.sh
@@ -0,0 +1,2 @@
+grep 'NOW GOING\|majority vote' result_2018 > MJ_information
+python extract_MJ_information.py
